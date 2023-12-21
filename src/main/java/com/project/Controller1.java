0bf79c7edc71f2b42d91ac38470ed10b96135c4c
Pablo Mejías <pablomejiast@gmail.com>
2023-12-21 19:10:33 +0100

    commit view 0 & 1

--- src/main/java/com/project/Controller1.java
+++ src/main/java/com/project/Controller1.java
@@ -1,45 +1,62 @@
 package com.project;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.PublicKey;
+import java.util.ResourceBundle;
+
+import javax.crypto.Cipher;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class Controller1 {
 
     @FXML
-    private Button button0, button1, button2;
-    @FXML
-    private AnchorPane container;
+    Button back, encrypt;
 
     @FXML
-    private void toView0(ActionEvent event) {
+    TextField publicKey, file, destination;
+
+    String keyDirectory = "src/main/resources/data/keys/";
+    String inDirectory = "src/main/resources/data/in/";
+    String outDirectory = "src/main/resources/data/out/";
+
+    @FXML
+    private void back(ActionEvent event) {
         UtilsViews.setView("View0");
     }
-    
-    @FXML
-    private void toView1(ActionEvent event) {
-        UtilsViews.setView("View1");
-    }
 
     @FXML
-    private void toView2(ActionEvent event) {
-        UtilsViews.setView("View2");
+    private void Encrypt(ActionEvent event){
+        if(publicKey.getText() != "" && file.getText() != "" && destination.getText() != ""){
+            try {
+                PublicKey publicKeyFile = loadPublicKey(keyDirectory + publicKey.getText());
+                String document = new String(Files.readAllBytes(Paths.get(inDirectory + file.getText())));
+                byte[] encryptedData = encryptData(document, publicKeyFile);
+                Files.write(Paths.get(outDirectory + destination.getText()), encryptedData);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    @FXML
-    private void animateToView0(ActionEvent event) {
-        UtilsViews.setViewAnimating("View0");
-    }
-    
-    @FXML
-    private void animateToView1(ActionEvent event) {
-        UtilsViews.setViewAnimating("View1");
+    public static PublicKey loadPublicKey(String filename) throws Exception {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+            return (PublicKey) ois.readObject();
+        }
     }
 
-    @FXML
-    private void animateToView2(ActionEvent event) {
-        UtilsViews.setViewAnimating("View2");
+    public static byte[] encryptData(String data, PublicKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data.getBytes());
     }
-
 }
--- src/main/java/com/project/Main.java
+++ src/main/java/com/project/Main.java
@@ -1,5 +1,14 @@
 package com.project;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openpgp.PGPKeyPair;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -25,15 +34,30 @@ public class Main extends Application {
         Scene scene = new Scene(UtilsViews.parentContainer);
         
         stage.setScene(scene);
-        stage.setTitle("Animació entre vistes");
+        stage.setTitle("Encrypt");
         stage.setMinWidth(windowWidth);
         stage.setMinHeight(windowHeight);
         stage.show();
+
+        Security.addProvider(new BouncyCastleProvider());
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA", "BC");
+        keyGen.initialize(2048, new SecureRandom());
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        try (ObjectOutputStream publicKeyOS = new ObjectOutputStream(new FileOutputStream("src/main/resources/data/keys/publicKey.key"))) {
+            publicKeyOS.writeObject(keyPair.getPublic());
+        }
+
+        try (ObjectOutputStream privateKeyOS = new ObjectOutputStream(new FileOutputStream("src/main/resources/data/keys/privateKey.key"))) {
+            privateKeyOS.writeObject(keyPair.getPrivate());
+        }
 
         // Add icon only if not Mac
         if (!System.getProperty("os.name").contains("Mac")) {
             Image icon = new Image("file:/icons/icon.png");
             stage.getIcons().add(icon);
         }
+        
     }
 }
